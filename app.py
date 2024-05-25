1b1a4249c74bec2584352089cf0f234ef3ac04c8
MR.CHAY <84063946+vin-stack@users.noreply.github.com>
2024-05-25 09:16:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ import io
 # Initialize the models and configurations
 quantization_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16)
 model_id = "llava-hf/llava-1.5-7b-hf"
-pipe = pipeline("text-generation", model=model_id, model_kwargs={"quantization_config": quantization_config})
-
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+pipe = pipeline("text-generation", model=model_id, model_kwargs={"quantization_config": quantization_config.to("cpu")})
 
 # Function to transcribe audio
 def transcribe(audio):
