6385a269fe30def30e9a53509ef4a7ebb97d3b3a
MR.CHAY <84063946+vin-stack@users.noreply.github.com>
2024-05-25 05:53:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,11 @@
 import torch
 from transformers import BitsAndBytesConfig, pipeline
 
-quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.float16,
-    device='cpu'
-)
+
 
 model_id = "llava-hf/llava-1.5-7b-hf"
 
-pipe = pipeline("image-to-text",
-                model=model_id,
-                model_kwargs={"quantization_config": quantization_config})
+pipe = pipeline("image-to-text", model=model_id)
 
 import whisper
 import warnings
